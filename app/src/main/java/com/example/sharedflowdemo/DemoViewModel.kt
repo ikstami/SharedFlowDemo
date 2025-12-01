0a2dce17874d2c491d658593c319cc6407d0e9ca
ikstami <k.mus00@yandex.ru>
2025-12-01 16:37:34 +0700

    first

---
+++ app/src/main/java/com/example/sharedflowdemo/DemoViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.sharedflowdemo
+import kotlinx.coroutines.delay
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+
+class DemoViewModel: ViewModel() {
+    private val _sharedFlow = MutableSharedFlow<Int>()
+    val sharedFlow = _sharedFlow.asSharedFlow()
+    init {
+        sharedFlowInit()
+    }
+    private fun sharedFlowInit() {
+        viewModelScope.launch {
+            for (i in 1..1000) {
+                delay(2000)
+                println("Emitting $i")
+                _sharedFlow.emit(i)
+            }
+        }
+    }
+
+}
